9394db229e56a9edc399f17ad9452800cd9cfd7f
Neciu Dan <dan@adoreme.com>
2021-01-25 10:18:51 +0100

    changed url

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,102 +1,150 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class AuthService {
-  constructor(private myRoute: Router,
-    private http: HttpClient) {}
+  constructor(private myRoute: Router, private http: HttpClient) {}
   sendToken(form) {
     const token = this.encrypt(form);
-    localStorage.setItem('token', token);
+    localStorage.setItem("token", token);
   }
   encrypt(form) {
-    return btoa(form.email + ':' + form.password);
+    return btoa(form.email + ":" + form.password);
   }
   getToken() {
-    return localStorage.getItem('token');
+    return localStorage.getItem("token");
   }
   isLoggednIn() {
     return this.getToken() !== null;
   }
   logout() {
-    localStorage.removeItem('token');
-    this.myRoute.navigate(['login']);
+    localStorage.removeItem("token");
+    this.myRoute.navigate(["login"]);
   }
   register(formValue) {
-    return this.http.post('http://34.89.187.127/api/v1/users/signup', formValue , { observe: 'response' });
+    return this.http.post(
+      "http://35.205.170.236/api/v1/users/signup",
+      formValue,
+      { observe: "response" }
+    );
   }
 
   editProfile(formValue) {
-    return this.http.put('http://34.89.187.127/api/v1/users/update', formValue);
+    return this.http.put(
+      "http://35.205.170.236/api/v1/users/update",
+      formValue
+    );
   }
 
   getUser() {
-    return this.http.get('http://34.89.187.127/api/v1/users/view');
+    return this.http.get("http://35.205.170.236/api/v1/users/view");
   }
 
   getLoaned() {
-    return this.http.get('http://34.89.187.127/api/v1/users/view/lent_books');
+    return this.http.get("http://35.205.170.236/api/v1/users/view/lent_books");
   }
 
   getAuthors(sandbox) {
-    return this.http.get('http://34.89.187.127/api/v1/authors/' + sandbox);
+    return this.http.get("http://35.205.170.236/api/v1/authors/" + sandbox);
   }
 
   getAuthor(sandbox, id) {
-    return this.http.get('http://34.89.187.127/api/v1/authors/' + sandbox + '/' + id);
+    return this.http.get(
+      "http://35.205.170.236/api/v1/authors/" + sandbox + "/" + id
+    );
   }
 
   updateAuthor(sandbox, id, formValue) {
-    return this.http.put('http://34.89.187.127/api/v1/authors/' + sandbox + '/' + id, formValue);
+    return this.http.put(
+      "http://35.205.170.236/api/v1/authors/" + sandbox + "/" + id,
+      formValue
+    );
   }
 
   createAuthor(sandbox, formValue) {
-    return this.http.post('http://34.89.187.127/api/v1/authors/' + sandbox , formValue, { observe: 'response' });
+    return this.http.post(
+      "http://35.205.170.236/api/v1/authors/" + sandbox,
+      formValue,
+      { observe: "response" }
+    );
   }
 
   deleteAuthor(sandbox, id) {
-    return this.http.delete('http://34.89.187.127/api/v1/authors/' + sandbox + '/' + id, { observe: 'response' });
+    return this.http.delete(
+      "http://35.205.170.236/api/v1/authors/" + sandbox + "/" + id,
+      { observe: "response" }
+    );
   }
 
   getBooks(sandbox) {
-    return this.http.get('http://34.89.187.127/api/v1/books/' + sandbox);
+    return this.http.get("http://35.205.170.236/api/v1/books/" + sandbox);
   }
 
   getBook(sandbox, id) {
-    return this.http.get('http://34.89.187.127/api/v1/books/' + sandbox + '/' + id);
+    return this.http.get(
+      "http://35.205.170.236/api/v1/books/" + sandbox + "/" + id
+    );
   }
 
   updateBook(sandbox, id, formValue) {
-    return this.http.put('http://34.89.187.127/api/v1/books/' + sandbox + '/' + id, formValue);
+    return this.http.put(
+      "http://35.205.170.236/api/v1/books/" + sandbox + "/" + id,
+      formValue
+    );
   }
 
   lendBook(sandbox, id, userId) {
-    return this.http.post('http://34.89.187.127/api/v1/books/' + sandbox + '/' + id + '/lend/' + userId, {},{ observe: 'response' });
+    return this.http.post(
+      "http://35.205.170.236/api/v1/books/" +
+        sandbox +
+        "/" +
+        id +
+        "/lend/" +
+        userId,
+      {},
+      { observe: "response" }
+    );
   }
 
   returnBook(sandbox, id, userId) {
-    return this.http.post('http://34.89.187.127/api/v1/books/' + sandbox + '/' + id + '/returned/' + userId, {},{ observe: 'response' });
+    return this.http.post(
+      "http://35.205.170.236/api/v1/books/" +
+        sandbox +
+        "/" +
+        id +
+        "/returned/" +
+        userId,
+      {},
+      { observe: "response" }
+    );
   }
 
   createBook(sandbox, formValue) {
-    return this.http.post('http://34.89.187.127/api/v1/books/' + sandbox , formValue,  { observe: 'response' });
+    return this.http.post(
+      "http://35.205.170.236/api/v1/books/" + sandbox,
+      formValue,
+      { observe: "response" }
+    );
   }
 
   deleteBook(sandbox, id) {
-    return this.http.delete('http://34.89.187.127/api/v1/books/' + sandbox + '/' + id, { observe: 'response' });
+    return this.http.delete(
+      "http://35.205.170.236/api/v1/books/" + sandbox + "/" + id,
+      { observe: "response" }
+    );
   }
 
   getRole(username) {
-    if (username.indexOf('_tester') > -1 ) {
-      return 'tester';
+    if (username.indexOf("_tester") > -1) {
+      return "tester";
     }
-    return 'user';
+    return "user";
   }
 
   getSandbox(username) {
-    if (username.indexOf('_tester') > -1 ) {
-      username = username.split('_')[0];
+    if (username.indexOf("_tester") > -1) {
+      username = username.split("_")[0];
     }
     return username;
   }
